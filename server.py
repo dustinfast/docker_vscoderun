7a1a7df72fe6edeae14e07ba3f5f2325d6743cc8
dustinfast <dustin.fast@hotmail.com>
2019-03-09 21:22:20 -0600

    Removed debug statements -DF

--- server.py
+++ server.py
@@ -5,14 +5,13 @@
     specified by EDITOR.
 """
 
-import socket
 import os
+import socket
 
 # Customizable constants
 HOST = '10.0.0.145'                 # Docker host IP (do NOT use localhost)
-HOST_PATH = '//c/dev/repos'         # Docker host volume directory
-CONTAINER_PATH = '/repos'           # Docker container mount point
-# TODO: OVERLAP_PATH = '/repos'             # Path overlap
+HOST_PATH = 'c:\\dev\\'             # Host volume directory (no trailing /)
+CONTAINER_PATH = '/repos'           # Container volume mount point
 EDITOR = 'code'                     # Editor command. Ex: 'code' or 'notepad'
 ALLOW_CONNS_FROM = ['10.0.0.145']   # Allowed IPs. If blank, allows all
 
@@ -22,11 +21,15 @@ MSG_SIZE = 1024                     # TCP Msg Size
 
 if __name__ == '__main__':
     # Init listener
-    sock = socket.socket()
-    sock.bind((HOST, LISTEN_PORT))
-    sock.listen(1)
+    try:
+        sock = socket.socket()
+        sock.bind((HOST, LISTEN_PORT))
+        sock.listen(1)
+    except OSError as e:
+        print('docker_vscoderun failed to bind.')
+        exit()
 
-    print('Listening on %s:%d...' % (HOST, LISTEN_PORT))
+    print('docker_vscoderun listening on %s:%d...' % (HOST, LISTEN_PORT))
 
     # Accept incoming connections indefinetely
     while True:
@@ -37,13 +40,21 @@ if __name__ == '__main__':
         # Process or ignore the request, depending on client address
         if not ALLOW_CONNS_FROM or client_ip in ALLOW_CONNS_FROM:
             fname = conn.recv(MSG_SIZE).decode()
-            print("Opening %s with %s from %s." % (fname, EDITOR, client_ip))
-            try:
-                os.system(EDITOR + ' ' + fname)
-            except Exception as e:
-                print(e)
+            fnamef = HOST_PATH + fname
+            # print("Opening %s with %s from %s." % (fnamef, EDITOR, client_ip))
+
+            # Ensure file is in container volume's mounted location
+            if not fname.startswith(CONTAINER_PATH):
+                # print('\tERROR: File is not located in container volume.')
+                pass
+            else:
+                try:
+                    os.system(EDITOR + ' ' + fnamef)
+                except Exception as e:
+                    print(e)
         else:
-            print('Ignored request from %s.' % client_ip)
+            # print('Ignored request from %s.' % client_ip)
+            pass
 
         conn.close()
 
